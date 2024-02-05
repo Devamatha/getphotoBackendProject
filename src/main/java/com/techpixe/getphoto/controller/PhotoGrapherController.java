29a0aa9f87b64f1354a33deaced205a281eb7f9b
prasad <sldprasad022@gmail.com>
2024-02-05 17:36:14 +0530

    commit photographer Details

--- src/main/java/com/techpixe/getphoto/controller/PhotoGrapherController.java
+++ src/main/java/com/techpixe/getphoto/controller/PhotoGrapherController.java
@@ -1,8 +1,12 @@
 package com.techpixe.getphoto.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -69,4 +73,40 @@ public class PhotoGrapherController
 	{
 		return emailOrMobileNumber.matches("\\d+");
 	}
+@GetMapping("/get/{photographer_Id}")
+	public ResponseEntity<?> fetchById(@PathVariable("photographer_Id") Long id)
+	{
+		PhotoGrapher fetchById = photoGrapherService.fetchById(id);
+		return ResponseEntity.ok(fetchById);
+	}
+	
+	@GetMapping("/getall")
+	public ResponseEntity<List<PhotoGrapher>> fetchAll()
+	{
+		List<PhotoGrapher> fetchAll = photoGrapherService.fetchAll();
+		return ResponseEntity.ok(fetchAll);
+	}
+	
+	@DeleteMapping("/delete/{photographer_Id}")
+	public ResponseEntity<Void> deleteById(@PathVariable("photographer_Id") Long id)
+	{
+		PhotoGrapher photoGrapher = photoGrapherService.fetchById(id);
+		if (photoGrapher==null) 
+		{
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		else
+		{
+			photoGrapherService.deleteById(id);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+		
+//	@PutMapping("/update/{photographer_Id}")
+//	public ResponseEntity<PhotoGrapher> updatePhotographer(@PathVariable("photographer_Id") Long id, @RequestBody PhotoGrapher photoGrapher)
+//	{
+//		Optional<PhotoGrapher> updatePhotographer = photoGrapherService.update(id, photoGrapher);
+//		return updatePhotographer.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+//	}
+	
 }
--- src/main/java/com/techpixe/getphoto/service/PhotoGrapherService.java
+++ src/main/java/com/techpixe/getphoto/service/PhotoGrapherService.java
@@ -1,13 +1,24 @@
 package com.techpixe.getphoto.service;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 
 import com.techpixe.getphoto.entity.PhotoGrapher;
 
-public interface PhotoGrapherService 
-{
-	PhotoGrapher registration(Long admin ,String email,Long mobileNumber,String fullName);
-	ResponseEntity<?> loginByMobileNumber(Long mobileNumber,String password);
+public interface PhotoGrapherService {
+	PhotoGrapher registration(Long admin, String email, Long mobileNumber, String fullName);
+
+	ResponseEntity<?> loginByMobileNumber(Long mobileNumber, String password);
+
 	ResponseEntity<?> loginByEmail(String email, String password);
-	
+
+	PhotoGrapher fetchById(Long id);
+
+	void deleteById(Long id);
+
+	List<PhotoGrapher> fetchAll();
+
+	// Optional<PhotoGrapher> update(Long id, PhotoGrapher photoGrapher);
+
 }
