9dd4364c8d0821a6c568fe1e1ce6cda32499f152
devamatha <p.devamatha2001@gmail.com>
2024-02-16 16:38:46 +0530

    changePassword and forgot Password

--- src/main/java/com/techpixe/getphoto/serviceImpl/AdminServiceImpl.java
+++ src/main/java/com/techpixe/getphoto/serviceImpl/AdminServiceImpl.java
@@ -107,88 +107,100 @@ public class AdminServiceImpl implements AdminService {
 		}
 	}
 
-//***************CHANGE PASSWORD*************************	
+	//***************CHANGE PASSWORD*************************	
 
-	@Override
-	public ResponseEntity<?> changePassword(Long admin_Id, String password, String confirmPassword) {
-		Admin user = adminRepository.findByPassword(password);
-		PhotoGrapher user1 = PhotoGrapherRepository.findByPassword(password);
-		if (user != null && user.getPassword().equals(password)) {
+		@Override
+		public ResponseEntity<?> changePassword(Long admin_Id, String password, String confirmPassword) {
+			Admin user = adminRepository.findById(admin_Id).orElse(null);
+			PhotoGrapher user1 = PhotoGrapherRepository.findById(admin_Id)
+			        .orElseThrow(() -> new RuntimeException("photographer is not present: " + admin_Id));
+			if (user != null && user.getPassword().equals(password)) {
 
-			AdminDto adminDto = new AdminDto();
-			adminDto.setAdmin_Id(user.getAdmin_Id());
-			adminDto.setFullName(user.getFullName());
-			adminDto.setEmail(user.getEmail());
-			adminDto.setMobileNumber(user.getMobileNumber());
-			adminDto.setRole(user.getRole());
-			adminDto.setPassword(confirmPassword);
+				AdminDto adminDto = new AdminDto();
+				adminDto.setAdmin_Id(user.getAdmin_Id());
+				adminDto.setFullName(user.getFullName());
+				adminDto.setEmail(user.getEmail());
+				adminDto.setMobileNumber(user.getMobileNumber());
+				adminDto.setRole(user.getRole());
+				adminDto.setPassword(confirmPassword);
 
-			user.setPassword(confirmPassword);
-			adminRepository.save(user);
-			return ResponseEntity.ok(adminDto);
+				user.setPassword(confirmPassword);
+				adminRepository.save(user);
+				return ResponseEntity.ok(adminDto);
 
-		} else if (user1 != null && user1.getPassword().equals(password)) {
+			} else if (user1 != null && user1.getPassword().equals(password)) {
 
-			PhotoGrapherDTo photoGrapherDTo = new PhotoGrapherDTo();
-			photoGrapherDTo.setPhotographer_Id(user1.getPhotographer_Id());
-			photoGrapherDTo.setFullName(user1.getFullName());
-			photoGrapherDTo.setEmail(user1.getEmail());
-			photoGrapherDTo.setMobileNumber(user1.getMobileNumber());
-			photoGrapherDTo.setPassword(confirmPassword);
-			photoGrapherDTo.setRole(user1.getRole());
+				PhotoGrapherDTo photoGrapherDTo = new PhotoGrapherDTo();
+				photoGrapherDTo.setPhotographer_Id(user1.getPhotographer_Id());
+				photoGrapherDTo.setFullName(user1.getFullName());
+				photoGrapherDTo.setEmail(user1.getEmail());
+				photoGrapherDTo.setMobileNumber(user1.getMobileNumber());
+				photoGrapherDTo.setPassword(confirmPassword);
+				photoGrapherDTo.setRole(user1.getRole());
 
-			user1.setPassword(confirmPassword);
-			PhotoGrapherRepository.save(user1);
-			return ResponseEntity.ok(photoGrapherDTo);
-		} else {
-			ErrorResponseDto error = new ErrorResponseDto();
-			error.setError("######################Password is not present#################");
-			// return ResponseEntity.internalServerError().body(error);
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+				user1.setPassword(confirmPassword);
+				PhotoGrapherRepository.save(user1);
+				return ResponseEntity.ok(photoGrapherDTo);
+			} else {
+				ErrorResponseDto error = new ErrorResponseDto();
+				error.setError("######################Password is not present#################");
+				// return ResponseEntity.internalServerError().body(error);
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+			}
 		}
-	}
 
-	// *************FORGOT PASSWORD*****************
+		// *************FORGOT PASSWORD*****************
 
-	@Override
-	public ResponseEntity<?> forgotPassword(String email) {
-		Admin admin = adminRepository.findByEmail(email);
-		PhotoGrapher photoGrapher = PhotoGrapherRepository.findByEmail(email);
+		@Override
+		public ResponseEntity<?> forgotPassword(String email) {
+			Admin admin = adminRepository.findByEmail(email);
+			PhotoGrapher photoGrapher = PhotoGrapherRepository.findByEmail(email);
 
-		if (admin != null) {
-			AdminDto adminDTO = new AdminDto();
-			adminDTO.setAdmin_Id(admin.getAdmin_Id());
-			adminDTO.setFullName(admin.getFullName());
-			adminDTO.setEmail(email);
-			adminDTO.setMobileNumber(admin.getMobileNumber());
-			adminDTO.setRole(admin.getRole());
+			if (admin != null) {
+				AdminDto adminDTO = new AdminDto();
+				adminDTO.setAdmin_Id(admin.getAdmin_Id());
+				adminDTO.setFullName(admin.getFullName());
+				adminDTO.setEmail(email);
+				adminDTO.setMobileNumber(admin.getMobileNumber());
+				adminDTO.setRole(admin.getRole());
 
-			String password = generatePassword();
-			adminDTO.setPassword(password);
-			admin.setPassword(password);
-			adminRepository.save(admin);
+				String password = generatePassword();
+				adminDTO.setPassword(password);
+				admin.setPassword(password);
+				adminRepository.save(admin);
 
-			return ResponseEntity.ok(adminDTO);
-		} else if (photoGrapher != null) {
-			PhotoGrapherDTo photoGrapherDTO = new PhotoGrapherDTo();
-			photoGrapherDTO.setPhotographer_Id(photoGrapher.getPhotographer_Id());
-			photoGrapherDTO.setFullName(photoGrapher.getFullName());
-			photoGrapherDTO.setEmail(email);
-			photoGrapherDTO.setMobileNumber(photoGrapher.getMobileNumber());
-			photoGrapherDTO.setRole(photoGrapher.getRole());
+				return ResponseEntity.ok(adminDTO);
+			} else if (photoGrapher != null) {
+				PhotoGrapherDTo photoGrapherDTO = new PhotoGrapherDTo();
+				photoGrapherDTO.setPhotographer_Id(photoGrapher.getPhotographer_Id());
+				photoGrapherDTO.setFullName(photoGrapher.getFullName());
+				photoGrapherDTO.setEmail(email);
+				photoGrapherDTO.setMobileNumber(photoGrapher.getMobileNumber());
+				photoGrapherDTO.setRole(photoGrapher.getRole());
 
-			String password = generatePassword();
-			photoGrapherDTO.setPassword(password);
-			photoGrapher.setPassword(password);
-			PhotoGrapherRepository.save(photoGrapher);
+				String password = generatePassword();
+				photoGrapherDTO.setPassword(password);
+				photoGrapher.setPassword(password);
+				PhotoGrapherRepository.save(photoGrapher);
+				
+				SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+				simpleMailMessage.setFrom(fromMail);
+				simpleMailMessage.setTo(email);
+				simpleMailMessage.setSubject("Registration completed Successfully in GetPhoto application\n");
+				simpleMailMessage.setText("Dear " +photoGrapher.getFullName()
+						+ "\n\nPlease check your  email and generted passowrd\n UserEmail  :"
+						+ email + "\n  MobileNumber :" + photoGrapher.getMobileNumber() + "\n New Password   :" + password
+						+ "\n\n"
+						+ "you will be required to reset the New password upon login\n\n\n if you have any question or if you would like to request a call-back,please email us at support info@techpixe.com");
+				javaMailSender.send(simpleMailMessage);
 
-			return ResponseEntity.ok(photoGrapherDTO);
-		} else {
-			ErrorResponseDto errorResponseDto = new ErrorResponseDto();
-			errorResponseDto.setError("Email is not matching");
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponseDto);
+				return ResponseEntity.ok(photoGrapherDTO);
+			} else {
+				ErrorResponseDto errorResponseDto = new ErrorResponseDto();
+				errorResponseDto.setError("Email is not matching");
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponseDto);
+			}
 		}
-	}
 
 	// **********Generate Random Password ********************
 	private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
